49ee304f7e46080471c99e98b389a6561064a254
Darren McPherson <dmcp@dmcp.home>
2020-02-23 23:19:50 +0000

    just removing some space

--- twitter.py
+++ twitter.py
@@ -3,15 +3,12 @@ import time
 from twitter_keys import API_KEY, API_SECRET_KEY, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
 auth = tweepy.OAuthHandler(API_KEY, API_SECRET_KEY)
-
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-
 user = api.me()
 
 search = 'JavaScript'
-
 no_of_tweets = 20
 
 for tweet in tweepy.Cursor(api.search, search).items(no_of_tweets):
